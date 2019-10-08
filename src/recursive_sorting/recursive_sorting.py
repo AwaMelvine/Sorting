9d1910faf03df57dcbe9daa53e76b735b567c99d
AwaMelvine <melvineawa@gmail.com>
2019-10-08 23:12:42 +0100

    Implement merge method

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,24 +1,37 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    lenA = len(arrA)
-    lenB = len(arrB)
-    elements = lenA + lenB
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
 
-    i = 0
-    while lenB >= 0 and lenA >= 0:
-        if arrA[lenA - 1 - i] > arrB[lenB - 1 - i]:
-            merged_arr[elements - 1 - i] = arrA[lenA - 1]
-        elif arrB[lenB - 1 - i] > arrA[lenA - 1 - i]:
-            merged_arr[elements - 1 - i] = arrB[lenB - 1]
-        lenB -= i
-        lenA -= i
-        i += 1
+    i = elements - 1
+    x = len(arrA) - 1
+    y = len(arrB) - 1
+    done = False
+
+    while not done:
+        if arrA[x] > arrB[y]:
+            merged_arr[i] = arrA[x]
+            if x >= 0:
+                x -= 1
+        else:
+            merged_arr[i] = arrB[y]
+            if y >= 0:
+                y -= 1
+
+        if x < 0 or y < 0:
+            done = True
+        i -= 1
+    merged_arr[0] = arrA[0] if arrA[0] < arrB[0] else arrB[0]
+    
     return merged_arr
 
 
+print(merge([1, 5], [0, 8, 90, 92]))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
+
+
 def merge_sort(arr):
     # TO-DO
     if len(arr) == 0:
